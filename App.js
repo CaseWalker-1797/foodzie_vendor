c71d22a5fa1c48b3f4dafb995fe2dd746cb35782
Case Walker <veerkamdar123@gmail.com>
2024-03-15 17:35:09 +0530

    123

---
+++ App.js
@@ -0,0 +1,15 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import AppNav from './src/navigation/appNav/AppNav';
+import CustomBottomTab from './src/navigation/appNav/CustomBottomTab';
+
+const App = () => {
+  return (
+    <SafeAreaProvider>
+      <CustomBottomTab/>
+    </SafeAreaProvider>
+  );
+};
+
+export default App;
---
+++ src/navigation/appNav/AppNav.js
@@ -0,0 +1,25 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Screen1 from '../../screens/appStack/Screen1';
+import Screen2 from '../../screens/appStack/Screen2';
+import Screen3 from '../../screens/appStack/Screen3';
+import Screen4 from '../../screens/appStack/Screen4';
+
+const Tab = createBottomTabNavigator();
+
+const AppNav = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{headerShown: false}}>
+        <Tab.Screen name="s1" component={Screen1} />
+        <Tab.Screen name="s2" component={Screen2} />
+        <Tab.Screen name="s3" component={Screen3} />
+        <Tab.Screen name="s4" component={Screen4} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNav;
